5fd2f38a9a745be1afbe637e2bd51cb2cd529a2f
sezgindurak <sezdurak@gmail.com>
2017-12-13 22:58:27 +0200

    Add files via upload

---
+++ kolayRunner.java
@@ -0,0 +1,22 @@
+package kolaySorular;
+
+public class kolayRunner {
+
+	public static void main(String[] args) {
+		//metodlar donguMetod = new metodlar();
+        //donguMetod.dongusel();
+        
+		// metodlar buyukkucuk = new metodlar();
+		// buyukkucuk.buyukKucuk();
+        
+        //metodlar besten = new metodlar();
+        //besten.bestenYirmiye();
+		
+		//metodlar negpoz = new metodlar();
+		//negpoz.negPoz();
+		
+		metodlar ucsayi = new metodlar();
+		ucsayi.ucsayi();
+	}
+
+}
